b259050995abc75274c45e30d5ae8ff80eef757a
jcomedouteau <jcome.douteau@gmail.com>
2016-01-03 20:21:55 +0100

    Vague + gameplay

--- BernyDeCompy/Assets/Scripts/Menu.cs
+++ BernyDeCompy/Assets/Scripts/Menu.cs
@@ -11,5 +11,19 @@ public class Menu : MonoBehaviour {
 		if (GUI.Button (new Rect (610, 140, 110, 20), "Surfons la vague")) {
 			Application.LoadLevel (1);
 		}
+
+		//Tuto
+		GUI.Box (new Rect (750, 100, 400, 180), "Tutoriel : \n" +
+			"Pour vous déplacer dans le jeu: \n" +
+			"Le tapis de danse permet de tourner son surf. \n" +
+			"L'oculus permet de rétablir l'équilibre sur le surf en penchant la tete. \n" +
+			"Des déséquilibres aléatoires viendront vous désiquilibrer. \n" +
+			"Plus vous avancez, plus vous gagnez des points. \n" +
+			"Plus vous gagnez de points, plus vous acccélerez. \n" +
+			"Attention, la vague vous suit, ne vous laissez pas rattraper. \n" +
+			"Les mamies rapportent des points bonus ! \n" +
+			"" +
+			"Bon surf !");
+
 	}
 }
